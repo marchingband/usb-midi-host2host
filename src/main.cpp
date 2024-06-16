8cdbd9d0a3e83cf914bb81e62cd02cdcaf9af392
Andrew March <youthclubrecords@gmail.com>
2024-06-16 09:29:40 -0700

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include "Arduino.h"
 #include "MIDIUSB.h"
 
-midiEventPacket_t rx;
-uint8_t uart_buf[4] = {0};
-size_t buf_p = 0;
+midiEventPacket_t rx;       // this type comes from the MIDIUSB library.
+uint8_t uart_buf[4] = {0};  // store the incomming UART data.
+size_t buf_p = 0;           // a pointer for the buffer.
 bool _blink = false;
 
 void blink(void){
@@ -14,21 +14,21 @@ void blink(void){
 void usb_device_loop(void)
 {
     rx = MidiUSB.read();
-	if (rx.header != 0){
-        Serial1.write((uint8_t *)&rx, 4);
+	if (rx.header != 0){                    // 0 header is just noise.
+        Serial1.write((uint8_t *)&rx, 4);   // midiEventPacket_t is a struct with 4 uint8_t's, so we can cast it to an array.
         blink();
 	} 
 }
 
 void midi_uart_loop(void){
     int data = Serial1.read();
-    if(data != -1){
-        uart_buf[buf_p] = (uint8_t)data;
-        buf_p++;
-        if(buf_p == 4){
+    if(data != -1){                         // -1 means no data.
+        uart_buf[buf_p] = (uint8_t)data;    // cast the int to a uint8_t and add it to the buffer.
+        buf_p++;                            // incerement the pointer.
+        if(buf_p == 4){                     // USB MIDI packets are always 4 bytes.
             MidiUSB.write(uart_buf, 4);
             MidiUSB.flush();
-            buf_p = 0;
+            buf_p = 0;                      // reset the pointer.
             blink();
         }
     }
@@ -36,7 +36,7 @@ void midi_uart_loop(void){
 
 void setup(){
     Serial1.begin(31250);
-    blink();
+    blink();                // turn off the LED to start.
 }
 
 void loop(){
